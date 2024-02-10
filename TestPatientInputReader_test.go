aa2baaab9fe8660913bddf6a8b41abf916af4780
vhatgadkar <vhatgadkar@yahoo.com>
2024-02-10 15:22:23 -0800

    Added Assert for test

--- TestPatientInputReader_test.go
+++ TestPatientInputReader_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
+
+	"gotest.tools/v3/assert"
 )
 
 // Test Emr JSON Patient record with struct defined in PatientInputReader file
@@ -59,5 +61,7 @@ func TestEmrStructPatientRecord(t *testing.T) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(jsonBytes))
+	jsonStr := string(jsonBytes)
+	testStr := "{\"resourceType\":\"Bundle\",\"id\":\"0c3151bd-1cbf-4d64-b04d-cd9187a4c6e0\",\"timestamp\":\"2021-04-02T12:12:21Z\",\"entry\":[{\"resource\":{\"resourceType\":\"Patient\",\"id\":\"6739ec3e-93bd-11eb-a8b3-0242ac130003\",\"active\":true,\"name\":[{\"text\":\"Tendo Tenderson\",\"family\":\"Tenderson\",\"given\":[\"Tendo\"]}],\"contact\":[{\"system\":\"phone\",\"value\":\"555-555-2021\",\"use\":\"mobile\"},{\"system\":\"email\",\"value\":\"tendo@tendoco.com\",\"use\":\"work\"}],\"gender\":\"female\",\"birthDate\":\"1955-01-06\",\"address\":[{\"use\":\"home\",\"line\":[\"2222 Home Street\"]}],\"type\":null,\"status\":\"\",\"subject\":{\"reference\":\"\"},\"actor\":{\"reference\":\"\"},\"period\":{\"start\":\"\",\"end\":\"\"},\"appointment\":{\"reference\":\"\"},\"code\":{\"coding\":null},\"meta\":{\"lastUpdated\":\"\"}}}]}"
+	assert.Equal(t, testStr, jsonStr)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module example/PatientInputReader
 
 go 1.21.3
+
+require (
+	github.com/google/go-cmp v0.5.9 // indirect
+	gotest.tools/v3 v3.5.1 // indirect
+)
