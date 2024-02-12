4e4c7564517445a6228bbee6c29527b04598981a
vhatgadkar <vhatgadkar@yahoo.com>
2024-02-12 10:44:15 -0800

    Move file write code to separate function

--- PatientMainApp.go
+++ PatientMainApp.go
@@ -15,15 +15,20 @@ func main() {
 	// Get patient feedback
 	patientFeedback := PatientInteraction(patientDetails)
 
-	// Save patient's feedback to a Json file well indented
+	// Save patient's feedback to a Json file
+	savePatientFeedback(patientFeedback)
+
+}
+
+// Save patient's feedback to a Json file well indented
+func savePatientFeedback(patientFeedback PatientFeedback) {
 	file, err := json.MarshalIndent(patientFeedback, "", " ")
 	if err != nil {
 		log.Fatal("Couldn't marshall feedback to file:", err)
 	}
 
-	err = os.WriteFile("saved-patient-feedback.json:", file, 0644)
+	err = os.WriteFile("saved-patient-feedback.json", file, 0644)
 	if err != nil {
 		log.Fatal("Couldn't save feedback to file:", err)
 	}
-
 }
